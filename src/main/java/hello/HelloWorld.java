ddcb67c7e0485bed27d4282c43394830307d69fa
axxle <kvv0911@gmail.com>
2019-10-10 11:48:06 +0500

    Создал build.gradle, сгенерировал и добавил в проект файлы gradle wrapper (gradlew), создал тестовое приложение на swing

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,30 @@
+package hello;
+
+import javax.swing.JFrame;
+
+public class HelloWorld extends JFrame { //Наследуя от JFrame мы получаем всю функциональность окна
+	
+	
+	
+	public static void main1(String[] args) {
+		System.out.println("Hello, World5!");
+	}
+
+
+
+  public HelloWorld(){
+    super("My First Window"); //Заголовок окна
+    setBounds(100, 100, 200, 200); //Если не выставить 
+                                   //размер и положение 
+                                   //то окно будет мелкое и незаметное
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //это нужно для того чтобы при 
+                                                    //закрытии окна закрывалась и программа,
+                                                    //иначе она останется висеть в процессах
+  }
+
+  public static void main(String[] args) { //эта функция может быть и в другом классе
+    HelloWorld app = new HelloWorld(); //Создаем экземпляр нашего приложения
+    app.setVisible(true); //С этого момента приложение запущено!
+	main1(null);
+  }
+}
